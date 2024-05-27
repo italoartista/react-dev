ea665d10dd76768cd2f73502b52c12f651b20cc5
Italo Adler <italo.adler@DIGITALCOLLEGE.LOCAL>
2024-05-27 17:12:54 -0300

    Add product component

---
+++ src/componentes/Product.jsx
@@ -0,0 +1,11 @@
+const Product = ({ product }) => {
+
+    return (
+        <div className="product">
+        <h2>{product.name}</h2>
+        <p>{product.price}</p>
+        </div>
+    );
+    }
+
+export default Product;
